492b9967a4d0e6b50e49e636cce61a8719a5fe12
qinghai <noteon@gmail.com>
2016-03-23 09:52:51 +0800

    sleep

--- start.sh
+++ start.sh
@@ -11,9 +11,8 @@ mongod --config /conf/mongod_2.conf
 mongod --config /conf/mongod_3.conf
 
 # mongo  --eval 'var config = { _id: "rs0", members: [ { _id: 0, host: "localhost:27017" }, { _id: 1, host: "localhost:27018" }, { _id: 3, host: "localhost:27019" } ] }; rs.initiate( config ); while (rs.status().startupStatus || (rs.status().hasOwnProperty("myState") && rs.status().myState != 1)) { printjson( rs.status() ); sleep(1000); }; printjson( rs.status() );'
-sleep 5000
 
-mongo --eval 'var rst=rs.initiate();  rs.add(rst.me.replace("27017","27018"));rs.add(rst.me.replace("27017","27018")); printjson(rs.status());'
+mongo --eval 'var rst=rs.initiate();sleep(5000); printjson(rst); rs.add(rst.me.replace("27017","27018"));sleep(1000);rs.add(rst.me.replace("27017","27018"));sleep(1000); printjson(rs.status());'
 
 # Run mongo as the running process, this is required to keep the docker process running
 mongo
